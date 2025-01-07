317070c980b79b6dc72ee0302c0c67fd9839abab
Andrej Titkov <ronya.titkov@gmail.com>
2025-01-07 19:08:40 +0200

    01.07 homework commit

---
+++ 06_praktiniai_arrays/06_01.Swap/src/main/java/Swap.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class Swap {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 3;
+        array[2] = 5;
+        array[3] = 7;
+        array[4] = 9;
+
+        int index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+        System.out.println("Give two indices to swap:");
+
+        int firstNum = Integer.parseInt(scanner.nextLine());
+        int temp = array[firstNum];
+        int secondNum = Integer.parseInt(scanner.nextLine());
+        array[firstNum] = array[secondNum];
+        array[secondNum] = temp;
+
+        System.out.println("");
+        index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+    }
+
+}
